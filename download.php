04a98c3a1eb707c6696dd00c408a6d502c074de6
Alex <vacheretal@eisti.eu>
2015-11-02 12:14:19 +0100

    Ajoute fichiers et images

---
+++ download.php
@@ -0,0 +1,32 @@
+<?php
+
+session_start();
+
+if (($_SESSION['algorithme'] == 'binarisation') || ($_SESSION['algorithme'] == 'detectionContoursSobel') || ($_SESSION['algorithme'] == 'detectionContoursLaplacien')) {
+	$_SESSION['nomImage'] = mb_strcut($_SESSION['nomImage'], 0, strlen($_SESSION['nomImage'])-4).'.pbm';
+}
+if (($_SESSION['algorithme'] == 'niveauGris') || ($_SESSION['algorithme'] == 'laplacien') || ($_SESSION['algorithme'] == 'reductionBruit')) {
+	$_SESSION['nomImage'] = mb_strcut($_SESSION['nomImage'], 0, strlen($_SESSION['nomImage'])-4).'.pgm';
+}
+
+$nom='/opt/lampp/htdocs/www/ServeurTraitementImage/Images/'.$_SESSION['algorithme'].'_'.$_SESSION['nomImage'];
+
+if(file_exists($nom)){
+	$fichier=basename($nom);
+
+	ob_start();
+	header('Content-Description: File Transfer');
+	header('Content-Type: application/octet-stream');
+	header('Expires: 0');
+	header('Content-Disposition: attachment; filename="'.basename($fichier).'"');
+	header('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+	header('Content-Length: '. filesize($nom));
+	ob_clean();
+	readfile($nom);
+	unlink($nom);
+	exit();
+
+}
+else
+	echo basename($nom);
+?>
---
+++ upload.php
@@ -0,0 +1,48 @@
+<?php
+
+session_start();
+
+$dossier = 'upload/';
+$_SESSION['nomImage'] = basename($_FILES['photo']['name']);
+// $tailleMax = 1000000;
+$extensionsPossible = array('.ppm', '.pbm', '.pgm');
+$extension = strrchr($_FILES['photo']['name'], '.'); 
+$_SESSION['algorithme'] = $_POST['algorithme'];
+
+
+if(!in_array($extension, $extensionsPossible)){
+	$erreur = 'Rentrer un type ppm, pbm ou pgm';
+}
+
+if((!isset($photo)) && (!isset($erreur))){
+
+	if(move_uploaded_file($_FILES['photo']['tmp_name'], $dossier.$_SESSION['nomImage'])){
+
+		if( $_SESSION['algorithme'] === "redimensionnement"){
+			exec('./STI '.$_SESSION['nomImage'].' '.$_SESSION['algorithme'].' '.$_POST['x1'].' '.$_POST['y1'].' '.$_POST['x2'].' '.$_POST['y2']);
+			unlink($dossier.$_SESSION['nomImage']);
+			header('location:index.php');
+			exit();
+		}
+		if( $_SESSION['algorithme'] === "masqueCutsom"){
+			exec('./STI '.$_SESSION['nomImage'].' '.$_SESSION['algorithme'].' '.$_POST['m1'].' '.$_POST['m2'].' '.$_POST['m3'].' '.$_POST['m4'].' '.$_POST['m5'].' '.$_POST['m6'].' '.$_POST['m7'].' '.$_POST['m8'].' '.$_POST['m9'].' '.'9');
+			unlink($dossier.$_SESSION['nomImage']);
+			header('location:index.php');
+			exit();
+		}
+		else{
+			exec('./STI '.$_SESSION['nomImage'].' '.$_SESSION['algorithme']);
+			unlink($dossier.$_SESSION['nomImage']);
+			header('location:index.php');
+			exit();
+		}
+	}
+	else{
+		echo 'Echec de l\'upload !';
+	}
+}
+else{
+	echo 'Erreur upload';
+}
+
+?>
