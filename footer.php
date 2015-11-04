447e666b9791d6244c3c3328f9ccafc39058591b
Erwan_Lbp <erwan.lbp@gmail.com>
2015-11-04 10:12:48 +0100

    Ajoute Header Footer

---
+++ footer.php
@@ -0,0 +1,21 @@
+<footer>
+	<!-- On affiche le nombre de connectés -->
+	<div class="gauche">
+		<p>
+			<?php echo 'Vous êtes actuellement <strong>37</strong> sur le site !'; ?>
+		</p>
+	</div>
+
+	<!-- On affiche le copyright du site -->
+	<div class="centre">
+		Développé par la dream team</br> Copyrights © 2015-2016 Tous droits réservés
+	</div>
+
+	<!-- On met deux liens, pour nous contacter ou voir l'équipe de développement -->
+	<div class="droite">
+		<p>
+			<a href="nousContacter.php">Nous contacter </a></li> | <a href="equipe.php">Equipe </a></li>
+		</p>
+	</div>
+	
+</footer>
