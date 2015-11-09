b80ad98f8b6d14a8a131b58babf580ec5e5e34e7
Alex <vacheretal@eisti.eu>
2015-11-09 09:54:38 +0100

    touch a tout

--- index.php
+++ index.php
@@ -20,76 +20,76 @@ session_start();
 	<?php include('header.php'); ?>	
 
 	<article>
-	<!-- 	Premier formulaire pour toutes les transformations qui ont pas besoin d'argument  -->
-	<fieldset>
-		<legend>Pas d'idée de légende ...</legend>
+
 		<form method="post" action="upload.php" enctype="multipart/form-data">
-			<p>Etape 1 : Choisir la transformation</p>
+			<fieldset>
+				<legend>Download l'image</legend>
+				<div id ="chercherImage">
+					<p>Etape 1 : Chercher la photo à transformer</p>
+					<input type="hidden" name="MAX_FILE_SIZE" value="taille">
+					<input type ="file" name ="photo" size ="taille"><br><br>
 
-			<div id ="formulaireTransfoDeBase">
-				<input type ="radio" name ="algorithme" value ="binarisation">Binarisation</option><br>
-				<input type ="radio" name ="algorithme" value ="niveauGris">Niveau de gris</option><br>
-				<input type ="radio" name ="algorithme" value ="symetrieH">Symétrie horizontale</option><br>
-				<input type ="radio" name ="algorithme" value ="symetrieV">Symétrie verticale</option><br>
-				<input type ="radio" name ="algorithme" value ="negatif">Négatif</option><br>
-				<input type ="radio" name ="algorithme" value ="ameliorationContraste">Amélioration du contraste</option><br>
-				<input type ="radio" name ="algorithme" value ="lissage">Lissage</option><br>
-				<input type ="radio" name ="algorithme" value ="laplacien">Laplacien</option><br>
-				<input type ="radio" name ="algorithme" value ="gradientSimple">Gradient Simple</option><br>
-				<input type ="radio" name ="algorithme" value ="gradientSobel">Gradient de Sobel</option><br>
-				<input type ="radio" name ="algorithme" value ="detectionContoursSobel">Detection de contours avec Sobel</option><br>
-				<input type ="radio" name ="algorithme" value ="detectionContoursLaplacien">Detection de contours avec le Laplacien</option><br>
-				<input type ="radio" name ="algorithme" value ="reductionBruit">Reduction de bruit</option><br>
-			</div>
-
-			<div id ="formulaireTransfoRedim">
-				<input type ="radio" id="redimensionnement" name ="algorithme" value ="redimensionnement">Redimensionnement</option><br><br>
-				<div id="redim">
-					Abscisse du 1er point <input class="champNombre" type ="number" name ="x1" value ="0" min="0"><br>
-					Ordonné du 1er point <input class="champNombre" type ="number" name ="y1" value ="0" min="0"><br><br>
-					Abscisse du 2eme point <input class="champNombre" type ="number" name ="x2" value ="0" min="0"><br>
-					Ordonné du 2eme point <input class="champNombre" type ="number" name ="y2" value ="0" min="0"><br><br>
 				</div>
-			</div>
+		</form>
 
-			<div id ="formulaireTransfoCustom">
-				<input id="masque" type ="radio" name ="algorithme" value ="masqueCustom">Masque Custom</option><br><br>
-				<div id="masqueCustom">
-					<p>Remplir la matrice suivante : </p>
-					<input class="champNombre" type ="number" name ="m1" value ="0">
-					<input class="champNombre" type ="number" name ="m2" value ="0">
-					<input class="champNombre" type ="number" name ="m3" value ="0"><br>
-					<input class="champNombre" type ="number" name ="m4" value ="0">
-					<input class="champNombre" type ="number" name ="m5" value ="0">
-					<input class="champNombre" type ="number" name ="m6" value ="0"><br>
-					<input class="champNombre" type ="number" name ="m7" value ="0">
-					<input class="champNombre" type ="number" name ="m8" value ="0">
-					<input class="champNombre" type ="number" name ="m9" value ="0"><br><br>
+		<form method="post" action="exec.php" enctype="multipart/form-data">
+				<p>Etape 3 : Choisir la transformation</p>
+
+				<div id ="formulaireTransfoDeBase">
+					<input type ="submit" name ="algorithme" value ="binarisation">
+					<input type ="submit" name ="algorithme" value ="niveauGris">
+					<input type ="submit" name ="algorithme" value ="symetrieH">
+					<input type ="submit" name ="algorithme" value ="symetrieV">
+					<input type ="submit" name ="algorithme" value ="negatif">
+					<input type ="submit" name ="algorithme" value ="ameliorationContraste">
+					<input type ="submit" name ="algorithme" value ="lissage">
+					<input type ="submit" name ="algorithme" value ="laplacien">
+					<input type ="submit" name ="algorithme" value ="gradientSimple">
+					<input type ="submit" name ="algorithme" value ="gradientSobel">
+					<input type ="submit" name ="algorithme" value ="detectionContoursSobel"><br>
+					<input type ="submit" name ="algorithme" value ="detectionContoursLaplacien"><br>
+					<input type ="submit" name ="algorithme" value ="reductionBruit"><br>
 				</div>
-			</div>
 
-			<div id ="chercherImage"
-			<p>Etape 2 : Chercher la photo à transformer</p>
-			<input type="hidden" name="MAX_FILE_SIZE" value="taille">
-			<input type ="file" name ="photo" size ="taille"><br><br>
+				<div id ="formulaireTransfoRedim">
+					<input type ="radio" id="redimensionnement" name ="algorithme" value ="redimensionnement">Redimensionnement</option><br><br>
+					<div id="redim">
+						Abscisse du 1er point <input class="champNombre" type ="number" name ="x1" value ="0" min="0"><br>
+						Ordonné du 1er point <input class="champNombre" type ="number" name ="y1" value ="0" min="0"><br><br>
+						Abscisse du 2eme point <input class="champNombre" type ="number" name ="x2" value ="0" min="0"><br>
+						Ordonné du 2eme point <input class="champNombre" type ="number" name ="y2" value ="0" min="0"><br><br>
+					</div>
+				</div>
 
-			<p>Etape 3 : Enjoy !</p>
-			<input type ="submit" name ="valider" value ="Envoyer la photo">
-		</div>
-	</form>
-</fieldset>
+				<div id ="formulaireTransfoCustom">
+					<input id="masque" type ="radio" name ="algorithme" value ="masqueCustom">Masque Custom</option><br><br>
+					<div id="masqueCustom">
+						<p>Remplir la matrice suivante : </p>
+						<input class="champNombre" type ="number" name ="m1" value ="0">
+						<input class="champNombre" type ="number" name ="m2" value ="0">
+						<input class="champNombre" type ="number" name ="m3" value ="0"><br>
+						<input class="champNombre" type ="number" name ="m4" value ="0">
+						<input class="champNombre" type ="number" name ="m5" value ="0">
+						<input class="champNombre" type ="number" name ="m6" value ="0"><br>
+						<input class="champNombre" type ="number" name ="m7" value ="0">
+						<input class="champNombre" type ="number" name ="m8" value ="0">
+						<input class="champNombre" type ="number" name ="m9" value ="0"><br><br>
+					</div>
+				</div>
+
+			</form>
+		</fieldset>
 
 
-<fieldset id="download">
-	<legend>Download l'image</legend>
-	<form method="post" action="download.php" enctype="multipart/form-data">
-		<p>Etape 4 : Récupérer la photo transformée</p>
-		<input type="submit" name="recuperer" value="Récupérer l'image">
-	</form>
-</fieldset>
+		<fieldset id="download">
+			<form method="post" action="download.php" enctype="multipart/form-data">
+				<p>Etape 4 : Récupérer la photo transformée</p>
+				<input type="submit" name="recuperer" value="Récupérer l'image">
+			</form>
+		</fieldset>
 
-</article>
+	</article>
 
-<?php include('footer.php'); ?>
+	<?php include('footer.php'); ?>
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -2,12 +2,56 @@
 
 session_start();
 
+switch ($_POST['algorithme']) {
+	case 'Binarisation'
+		$_SESSION['algorithme'] = 'binarisation';
+		break;
+	case 'Niveau de gris'
+		$_SESSION['algorithme'] = 'niveauGris';
+		break;
+		case 'Symetrie Horizontale'
+		$_SESSION['algorithme'] = 'symetrieH';
+		break;
+		case 'Symetrie Verticale'
+		$_SESSION['algorithme'] = 'symetrieV';
+		break;
+		case 'Negatif'
+		$_SESSION['algorithme'] = 'negatif';
+		break;
+		case 'Amelioration du contraste'
+		$_SESSION['algorithme'] = 'ameliorationContraste';
+		break;
+		case 'Lissage'
+		$_SESSION['algorithme'] = 'lissage';
+		break;
+		case 'Laplacien'
+		$_SESSION['algorithme'] = 'laplacien';
+		break;
+		case 'Gradient Simple'
+		$_SESSION['algorithme'] = 'gradientSimple';
+		break;
+		case 'Gradient Sobel'
+		$_SESSION['algorithme'] = 'gradientSobel';
+		break;
+		case 'Detection contours Sobel'
+		$_SESSION['algorithme'] = 'detectionContoursSobel';
+		break;
+		case 'Detection contours Laplacien'
+		$_SESSION['algorithme'] = 'detectionContoursLaplacien';
+		break;
+		case 'Reduction de bruit'
+		$_SESSION['algorithme'] = 'reductionBruit';
+		break;
+	default:
+		$_SESSION['algorithme'] = '';
+		break;
+}
+
 $dossier = 'Images/';
-$_SESSION['nomImage'] = basename($_FILES['photo']['name']);
+$_SESSION['cheminImage'] = ($_FILES['photo']['name']);
 // $tailleMax = 1000000;
 $extensionsPossible = array('.ppm', '.pbm', '.pgm');
 $extension = strrchr($_FILES['photo']['name'], '.'); 
-$_SESSION['algorithme'] = $_POST['algorithme'];
 
 
 if(!in_array($extension, $extensionsPossible)){
@@ -16,23 +60,23 @@ if(!in_array($extension, $extensionsPossible)){
 
 if((!isset($photo)) && (!isset($erreur))){
 
-	if(move_uploaded_file($_FILES['photo']['tmp_name'], $dossier.$_SESSION['nomImage'])){
+	if(move_uploaded_file($_FILES['photo']['tmp_name'], $dossier. basename($_SESSION['cheminImage']))){
 
 		if( $_SESSION['algorithme'] === "redimensionnement"){
-			exec('./STI '.$_SESSION['nomImage'].' '.$_SESSION['algorithme'].' '.$_POST['x1'].' '.$_POST['y1'].' '.$_POST['x2'].' '.$_POST['y2']);
-			unlink($dossier.$_SESSION['nomImage']);
+			exec('./STI '.basename($_SESSION['cheminImage']).' '.$_SESSION['algorithme'].' '.$_POST['x1'].' '.$_POST['y1'].' '.$_POST['x2'].' '.$_POST['y2']);
+			unlink($dossier.basename($_SESSION['cheminImage']));
 			header('location:index.php');
 			exit();
 		}
 		else if( $_SESSION['algorithme'] === "masqueCutsom"){
-			exec('./STI '.$_SESSION['nomImage'].' '.$_SESSION['algorithme'].' '.$_POST['m1'].' '.$_POST['m2'].' '.$_POST['m3'].' '.$_POST['m4'].' '.$_POST['m5'].' '.$_POST['m6'].' '.$_POST['m7'].' '.$_POST['m8'].' '.$_POST['m9'].' '.'9');
-			unlink($dossier.$_SESSION['nomImage']);
+			exec('./STI '.basename($_SESSION['cheminImage']).' '.$_SESSION['algorithme'].' '.$_POST['m1'].' '.$_POST['m2'].' '.$_POST['m3'].' '.$_POST['m4'].' '.$_POST['m5'].' '.$_POST['m6'].' '.$_POST['m7'].' '.$_POST['m8'].' '.$_POST['m9'].' '.'9');
+			unlink($dossier.basename($_SESSION['cheminImage']));
 			header('location:index.php');
 			exit();
 		}
 		else{
-			exec('./STI '.$_SESSION['nomImage'].' '.$_SESSION['algorithme']);
-			unlink($dossier.$_SESSION['nomImage']);
+			exec('./STI '.basename($_SESSION['cheminImage']).' '.$_SESSION['algorithme']);
+			unlink($dossier.basename($_SESSION['cheminImage']));
 			header('location:index.php');
 			exit();
 		}
