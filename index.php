0452bb2ceaf2dd23208398025f3539cbc7a2da06
Alex <vacheretal@eisti.eu>
2015-11-10 09:05:12 +0100

    Modifie bouton

--- index.php
+++ index.php
@@ -22,68 +22,71 @@ session_start();
 	<article>
 
 		<fieldset>
-			<form method="post" action="upload.php" enctype="multipart/form-data">
-				<legend>Download l'image</legend>
-				<div id ="chercherImage">
-					<input type="hidden" name="MAX_FILE_SIZE" value="taille">
-					<input type ="file" id="bouttonFile" name ="photo" size ="taille" onchange="this.form.submit()"><br><br>
+			<div id="grandeDivDroite">
+				<form method="post" action="upload.php" enctype="multipart/form-data">
+					<div id ="chercherImage">
+						<input type="hidden" name="MAX_FILE_SIZE" value="taille">
+						<input type ="file" id="bouttonFile" name ="photo" size ="taille" onchange="this.form.submit()"><br><br>
 
 
+					</div>
+				</form>
+
+				<div id="CanvasImage">
+					<?php 
+					if(isset($_SESSION['cheminImageJPG']))
+						echo "<img src='".$_SESSION['cheminImageJPG']."' alt='".basename($_SESSION['cheminImage'])."'>";
+					?>
 				</div>
-			</form>
-
-			<div id="CanvasImage">
-				<?php 
-				if(isset($_SESSION['cheminImageJPG']))
-					echo "<img src='".$_SESSION['cheminImageJPG']."' alt='".basename($_SESSION['cheminImage'])."'>";
-				?>
 			</div>
 
-			<form method="post" action="execTransfo.php" enctype="multipart/form-data">
+			<div id="grandeDivGauche">
+				<form method="post" action="execTransfo.php" enctype="multipart/form-data">
 
-				<div id ="formulaireTransfoDeBase">
-					<input type ="submit" name ="algorithme" value ="Binarisation">
-					<input type ="submit" name ="algorithme" value ="Niveau de gris">
-					<input type ="submit" name ="algorithme" value ="Symetrie Horizontale">
-					<input type ="submit" name ="algorithme" value ="Symetrie Verticale">
-					<input type ="submit" name ="algorithme" value ="Negatif">
-					<input type ="submit" name ="algorithme" value ="Amelioration du contraste">
-					<input type ="submit" name ="algorithme" value ="Lissage">
-					<input type ="submit" name ="algorithme" value ="Laplacien">
-					<input type ="submit" name ="algorithme" value ="Gradient Simple">
-					<input type ="submit" name ="algorithme" value ="Gradient Sobel">
-					<input type ="submit" name ="algorithme" value ="Detection contours Laplacien"><br>
-					<input type ="submit" name ="algorithme" value ="detectionContoursLaplacien"><br>
-					<input type ="submit" name ="algorithme" value ="Reduction de bruit"><br>
-				</div>
-
-				<div id ="formulaireTransfoRedim">
-					<input type ="radio" id="redimensionnement" name ="algorithme" value ="redimensionnement">Redimensionnement</option><br><br>
-					<div id="redim">
-						Abscisse du 1er point <input class="champNombre" type ="number" name ="x1" value ="0" min="0"><br>
-						Ordonné du 1er point <input class="champNombre" type ="number" name ="y1" value ="0" min="0"><br><br>
-						Abscisse du 2eme point <input class="champNombre" type ="number" name ="x2" value ="0" min="0"><br>
-						Ordonné du 2eme point <input class="champNombre" type ="number" name ="y2" value ="0" min="0"><br><br>
+					<div id ="formulaireTransfoDeBase">
+						<input class="buttonSubmit" type ="submit" name ="algorithme" value ="Binarisation">
+						<input class="buttonSubmit" type ="submit" name ="algorithme" value ="Niveau de gris">
+						<input class="buttonSubmit" type ="submit" name ="algorithme" value ="Symetrie Horizontale">
+						<input class="buttonSubmit" type ="submit" name ="algorithme" value ="Symetrie Verticale">
+						<input class="buttonSubmit" type ="submit" name ="algorithme" value ="Negatif">
+						<input class="buttonSubmit" type ="submit" name ="algorithme" value ="Amelioration du contraste">
+						<input class="buttonSubmit" type ="submit" name ="algorithme" value ="Lissage">
+						<input class="buttonSubmit" type ="submit" name ="algorithme" value ="Laplacien">
+						<input class="buttonSubmit" type ="submit" name ="algorithme" value ="Gradient Simple">
+						<input class="buttonSubmit" type ="submit" name ="algorithme" value ="Gradient Sobel">
+						<input class="buttonSubmit" type ="submit" name ="algorithme" value ="Detection contours Laplacien"><br>
+						<input class="buttonSubmit" type ="submit" name ="algorithme" value ="detectionContoursLaplacien"><br>
+						<input class="buttonSubmit" type ="submit" name ="algorithme" value ="Reduction de bruit"><br>
 					</div>
-				</div>
 
-				<div id ="formulaireTransfoCustom">
-					<input id="masque" type ="radio" name ="algorithme" value ="masqueCustom">Masque Custom</option><br><br>
-					<div id="masqueCustom">
-						<p>Remplir la matrice suivante : </p>
-						<input class="champNombre" type ="number" name ="m1" value ="0">
-						<input class="champNombre" type ="number" name ="m2" value ="0">
-						<input class="champNombre" type ="number" name ="m3" value ="0"><br>
-						<input class="champNombre" type ="number" name ="m4" value ="0">
-						<input class="champNombre" type ="number" name ="m5" value ="0">
-						<input class="champNombre" type ="number" name ="m6" value ="0"><br>
-						<input class="champNombre" type ="number" name ="m7" value ="0">
-						<input class="champNombre" type ="number" name ="m8" value ="0">
-						<input class="champNombre" type ="number" name ="m9" value ="0"><br><br>
+					<div id ="formulaireTransfoRedim">
+						<input type ="radio" id="redimensionnement" name ="algorithme" value ="redimensionnement">Redimensionnement</option><br><br>
+						<div id="redim">
+							Abscisse du 1er point <input class="champNombre" type ="number" name ="x1" value ="0" min="0"><br>
+							Ordonné du 1er point <input class="champNombre" type ="number" name ="y1" value ="0" min="0"><br><br>
+							Abscisse du 2eme point <input class="champNombre" type ="number" name ="x2" value ="0" min="0"><br>
+							Ordonné du 2eme point <input class="champNombre" type ="number" name ="y2" value ="0" min="0"><br><br>
+						</div>
 					</div>
-				</div>
 
-			</form>
+					<div id ="formulaireTransfoCustom">
+						<input id="masque" type ="radio" name ="algorithme" value ="masqueCustom">Masque Custom</option><br><br>
+						<div id="masqueCustom">
+							<p>Remplir la matrice suivante : </p>
+							<input class="champNombre" type ="number" name ="m1" value ="0">
+							<input class="champNombre" type ="number" name ="m2" value ="0">
+							<input class="champNombre" type ="number" name ="m3" value ="0"><br>
+							<input class="champNombre" type ="number" name ="m4" value ="0">
+							<input class="champNombre" type ="number" name ="m5" value ="0">
+							<input class="champNombre" type ="number" name ="m6" value ="0"><br>
+							<input class="champNombre" type ="number" name ="m7" value ="0">
+							<input class="champNombre" type ="number" name ="m8" value ="0">
+							<input class="champNombre" type ="number" name ="m9" value ="0"><br><br>
+						</div>
+					</div>
+
+				</form>
+			</div>
 		</fieldset>
 
 
