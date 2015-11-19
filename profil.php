4cc0d4d16f17ccb73bd20c1ef2e8e4cdb080eb0e
Vincent <romanetvin@eisti.eu>
2015-11-19 11:23:15 +0100

    Ajoute supprimer sur le profil

--- profil.php
+++ profil.php
@@ -38,8 +38,8 @@ include('connectBDD.php');
 
 
 		while($donnee = $req->fetch()){
-			echo '<div class="col-lg-2" style="border:solid;">';
-			echo '<img class="img-rounded" src="'.$donnee['chemin'].'" alt="Photos" >';
+			echo '<div class="col-lg-2">';
+			echo '<img class="img-rounded" src="'.mb_strcut($donnee['chemin'], 0, strlen($donnee['chemin'])-4).'.jpg" alt="'.$donnee['chemin'].'" >';
 			echo '<form method="post" action="supprimer.php" enctype="multipart/form-data">';
 			echo '	<input type="hidden" name="chemin" value="'.$donnee['chemin'].'">';
 			echo '	<input type="hidden" name="idImage" value="'.$donnee['id'].'">';
--- supprimer.php
+++ supprimer.php
@@ -1,6 +1,8 @@
 <?php 
 session_start();
 
+include('connectBDD.php');
+
 //Pour éviter qu'un mec pas connecté accède à la page avec la barre URL
 if(!isset($_SESSION['pseudo'])){
 	header("Location: index.php");
