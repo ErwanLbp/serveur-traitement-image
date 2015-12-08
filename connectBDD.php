92822b1bafc4e9faa49a17b185dc555165df12ef
Erwan_Lbp <erwan.lbp@gmail.com>
2015-12-08 14:09:52 +0100

    Met le fichier connectBDD bien

--- connectBDD.php
+++ connectBDD.php
@@ -1,10 +1,22 @@
 <!-- Connection à la base de données -->
 <?php 
 	try{ //On se connecte à la BDD, les messages d'erreur seront mieux expliqués
-		$bdd = new PDO('mysql:host=localhost;dbname=site_STI', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	if($_SERVER['SERVER_NAME'] == 'localhost')
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=site_STI', 'root', '');
+	}			
+	elseif($_SERVER['SERVER_NAME'] == 'cocplanifieur.netii.net')
+	{
+		$bdd = new PDO('mysql:host=mysql7.000webhost.com;dbname=a8602457_STI', 'a8602457_sti', 'a1z2e3r4t5y6',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	}
-	catch(Exception $e){
+	else
+	{
+		throw new Exception('Oula comment tu as eu ce site toi ?');
+	}
+
+}
+catch(Exception $e){
 		//Si il y a un problème on évite d'afficher le mot de passe, ca serait bete...
-		die('Erreur : '.$e->getMessage());
-	}
+	die('Erreur : '.$e->getMessage());
+}
 ?>
