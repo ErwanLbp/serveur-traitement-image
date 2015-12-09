af485e4cc9533500d7c9934b092e52c6335c6073
Erwan_Lbp <erwan.lbp@gmail.com>
2015-12-09 11:23:28 +0100

    .

--- connectBDD.php
+++ connectBDD.php
@@ -1,22 +1,12 @@
 <!-- Connection à la base de données -->
 <?php 
-	try{ //On se connecte à la BDD, les messages d'erreur seront mieux expliqués
-	if($_SERVER['SERVER_NAME'] == 'localhost')
-	{
-		$bdd = new PDO('mysql:host=localhost;dbname=site_STI', 'root', '');
-	}			
-	elseif($_SERVER['SERVER_NAME'] == 'cocplanifieur.netii.net')
-	{
-		$bdd = new PDO('mysql:host=mysql7.000webhost.com;dbname=a8602457_STI', 'a8602457_sti', 'a1z2e3r4t5y6',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-	}
-	else
-	{
-		throw new Exception('Oula comment tu as eu ce site toi ?');
-	}
-
+try{ 
+	// A changer si la connexion à phpmyadmin ne se fait pas avec ce login
+	// $bdd = new PDO('mysql:host=<HOTE>;dbname=<NOM BDD>', '<LOGIN>', '<PASSWORD>');
+	$bdd = new PDO('mysql:host=localhost;dbname=site_STI', 'root', '');
 }
 catch(Exception $e){
-		//Si il y a un problème on évite d'afficher le mot de passe, ca serait bete...
+	//Si il y a un problème on évite d'afficher le mot de passe, ca serait bete...
 	die('Erreur : '.$e->getMessage());
 }
 ?>
