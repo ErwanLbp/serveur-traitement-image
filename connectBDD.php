8f18d7151f45d9e0492fe6541ace9ab0b06b317c
Erwan_Lbp <erwan.lbp@gmail.com>
2015-12-16 13:45:42 +0100

    .

--- connectBDD.php
+++ connectBDD.php
@@ -3,10 +3,10 @@
 try{ 
 	// A changer si la connexion à phpmyadmin ne se fait pas avec ce login
 	// $bdd = new PDO('mysql:host=<HOTE>;dbname=<NOM BDD>', '<LOGIN>', '<PASSWORD>');
-	if($_SERVER['remote'] = 'localhost')
-		$bdd = new PDO('mysql:host=localhost;dbname=site_STI', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-	elseif ($_SERVER['remote'] = 'koding.io') 
+	if ($_SERVER['SERVER_NAME'] = 'erwanlbp.koding.io') 
 		$bdd = new PDO('mysql:host=localhost;dbname=site_STI', 'root', 'a1z2e3r4t5y6',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	elseif($_SERVER['SERVER_NAME'] = 'localhost')
+		$bdd = new PDO('mysql:host=localhost;dbname=site_STI', 'root', '',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	else
 		echo 'Probleme connexion BDD ! ';
 }
